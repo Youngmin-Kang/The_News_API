e7eb38c2fd63a8ab0e7ec48b1d75607999bf7be7
Jonathan-Kang <ymkang1630@gmail.com>
2021-12-07 11:31:16 -0300

    chore(Project): initialize

---
+++ src/test/java/cl/ucn/disc/dsm/ykang/newsapi/TheNewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ucn.disc.dsm.ykang.newsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheNewsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
